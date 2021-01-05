f9ee105b31145345ee3500cd74c679f17fe0eca2
朱彤 <1251921644@qq.com>
2021-01-05 10:21:37 +0800

    根据数组构造二叉树版本二

--- binaryTreeLevelOrder.cpp
+++ binaryTreeLevelOrder.cpp
@@ -40,37 +40,48 @@ public:
 
 
 
-void createTree( TreeNode *root, vector<int> num, int nodePlace){
-    if(num[2*nodePlace-1] != 0) {
-        root->left = (TreeNode *) malloc(sizeof(TreeNode));
-        root->left->val = num[2*nodePlace-1];
-        createTree(root->left, num, 2*nodePlace);
-    }
-    if(num[2*nodePlace] != 0) {
-        root->right = (TreeNode *) malloc(sizeof(TreeNode));
-        root->right->val = num[2*nodePlace];
-        createTree(root->right, num, 2*nodePlace+1);
-    }
-
-}
-
-TreeNode* createBinaryTree(vector<int> tree){
-    TreeNode *root;
-    root = (TreeNode *)malloc(sizeof(TreeNode));
-    int nodePlace = 1;
-    if(tree[0] == 0)
-        return NULL;
-    root->val = tree[0];
-    createTree(root,tree,1);
-    return root;
+//void createTree( TreeNode *root, vector<int> num, int nodePlace){
+//    if(num[2*nodePlace-1] != 0) {
+//        root->left = (TreeNode *) malloc(sizeof(TreeNode));
+//        root->left->val = num[2*nodePlace-1];
+//        createTree(root->left, num, 2*nodePlace);
+//    }
+//    if(num[2*nodePlace] != 0) {
+//        root->right = (TreeNode *) malloc(sizeof(TreeNode));
+//        root->right->val = num[2*nodePlace];
+//        createTree(root->right, num, 2*nodePlace+1);
+//    }
+//
+//}
+//
+//TreeNode* createBinaryTree(vector<int> tree){
+//    TreeNode *root;
+//    if(tree[0] == 0)
+//        return NULL;
+//    root = (TreeNode *)malloc(sizeof(TreeNode));
+//    int nodePlace = 1;
+//    root->val = tree[0];
+//    createTree(root,tree,1);
+//    return root;
+//}
+void createTree(TreeNode *&root,  vector<int> tree ,int treePlace){
+    if(tree[treePlace-1] == 0 || treePlace>tree.size())
+        return;
+    root = static_cast<TreeNode *>(malloc(sizeof(TreeNode)));
+    root->val = tree[treePlace-1];
+    createTree(root->left, tree, 2*treePlace);
+    createTree(root->right, tree, 2*treePlace + 1);
+    return;
 }
 
 int main(){
     vector<int> tree = {3,9,20,NULL,NULL,15,7};
-    TreeNode *root;
-    root = createBinaryTree(tree);
-    Solution solution;
-    solution.levelOrder(root);
+//    TreeNode *root;
+//    root = createBinaryTree(tree);
+//    Solution solution;
+//    solution.levelOrder(root);
+TreeNode *treeNode = NULL;
+createTree(treeNode, tree, 1);
     return 0;
 }
 
